f7179cfb0c7508dabea8d9f1856fb44e96509fb9
Abdul Ahad Khan <aakabdulahad7@gmail.com>
2024-08-03 20:13:38 +0500

    Header added and modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Header from './components/Header'
 import Navbar from './components/Navbar'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
      <Navbar title={"KHWARAKA"}/>
+     <Header/>
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <>
+      <div className="header">
+        <input id="search-area" type="text" placeholder="Search for foods" />
+        <button>Search</button>
+      </div>
+    </>
+  );
+}
